c169ae99dbaa1cc268a6566e0aab0ab59cdaa9f2
rocket-pocket <verdudaniel7@gmail.com>
2018-09-04 12:50:23 +0200

    model

---
+++ 03_formsAvanzado/Form.model.js
@@ -0,0 +1,16 @@
+class FormularioModel{
+    constructor(){
+        this.opciones = {
+            conditions: '',
+            turn: '',
+            curse: ''
+        },
+        this.user = { 
+            name: '', 
+            f_name: ''},
+        this.email= '',
+        this.passw = '',
+        this.coments = '',
+        this.aficiones= []
+    }
+}
