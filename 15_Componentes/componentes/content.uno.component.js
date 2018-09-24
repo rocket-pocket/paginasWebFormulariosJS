01cdd60ebde92517ff19498075c34b089f4bad1a
rocket-pocket <verdudaniel7@gmail.com>
2018-09-24 09:10:57 +0200

    añadir clases

--- 15_Componentes/componentes/content.uno.component.js
+++ 15_Componentes/componentes/content.uno.component.js
@@ -4,6 +4,7 @@ export class ContentUnoComponent{
     }
     render(){
         const div = document.createElement('section')/* como se le pone un class?? */
+        div.className = 'holam'
         div.innerHTML = `<article><p>hola soy ${this.msg}, mira que imágen más                          chula</p><img src="./assets/Monster.svg" alt=""></article>
                         `
         return div
